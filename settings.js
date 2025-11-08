c8dcec3ae341a7b33653f04a1abe2af587573f60
mominhuyaar <141264297+mominhuyaar@users.noreply.github.com>
2025-11-09 02:42:02 +0500

    Update bot settings for 'Moon Knight Bot'

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Moon Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Moon Knight Bot",
+  botOwner: 'Moon', // Your name
+  ownerNumber: '923030858661', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
